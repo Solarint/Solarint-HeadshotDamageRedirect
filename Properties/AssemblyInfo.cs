63964f6d0eb7525fc26700f11bcc808015f49c70
Solarint <fear231@gmail.com>
2024-07-29 23:28:32 -0500

    Revert "v 1.0"
    This reverts commit dcc1523fb9889d49f40589048314347308848ed1.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Solarint-GrenadeIndicator")]
+[assembly: AssemblyTitle("Solarint-HeadshotDamageRedirect")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Solarint-GrenadeIndicator")]
+[assembly: AssemblyProduct("Solarint-HeadshotDamageRedirect")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
